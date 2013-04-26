6ec2fab6b38b44f4825d49c0250f9b3576e12e82
Jonathan Guan <jonaguan@yahoo.com>
2013-04-26 16:14:22 -0700

    remove unitCache

--- war/js/mentor.js
+++ war/js/mentor.js
@@ -2,15 +2,48 @@
 
   
 
-
+// Load
 function onLinkedInLoad() {
      IN.Event.on(IN, "auth", onLinkedInAuth);
 }
+
+//Authorization
 function onLinkedInAuth() {
-     IN.API.Profile("me").result(displayProfiles);
-}
+	  IN.API.Connections("me")
+	    .fields("firstName", "lastName", "industry")
+	    .result(displayConnections)
+	    .error(displayConnectionsErrors);
+	}
+
+// Connections
+function displayConnections(connections) {
+	  var connectionsDiv = document.getElementById("connections");
+
+	  var members = connections.values; // The list of members you are connected to
+	  for (var member in members) {
+	    connectionsDiv.innerHTML += "<p>" + members[member].firstName + " " + members[member].lastName
+	      + " works in the " + members[member].industry + " industry";
+	  }     
+	}
+
+
+function displayConnectionsErrors(error) { /* do nothing */ }
+	
+// Profiles
 function displayProfiles(profiles) {
-     member = profiles.values[0];
-     document.getElementById("profiles").innerHTML = 
-          "<p id=\"" + member.id + "\">Hello " +  member.firstName + " " + member.lastName + "</p>";
-}
+	  var profilesDiv = document.getElementById("profiles");
+
+	  var members = profiles.values;
+	  for (var member in members) {
+	    profilesDiv.innerHTML += "<p>" + members[member].firstName + " " + members[member].lastName 
+	      + " works in the " + members[member].industry + " industry.";
+	  }
+	}
+
+
+function displayProfilesErrors(error) {
+	  profilesDiv = document.getElementById("profiles");
+	  profilesDiv.innerHTML = "<p>Oops! An invalid profile was detected!</p>";
+	  console.log(error);
+	}
+	   
