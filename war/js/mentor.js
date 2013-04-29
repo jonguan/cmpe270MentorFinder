9ac0ed0df8b1d234493300cadd4adb1cc585bcb5
Jonathan Guan <jonaguan@yahoo.com>
2013-04-29 06:04:34 -0700

    relevant search

--- war/js/mentor.js
+++ war/js/mentor.js
@@ -1,3 +1,10 @@
+/*
+ * Global variables
+ */
+var memberSelf;
+var start = 0;
+var page = 0;
+var numPerPage = 20;
 
 /*
  * Load
@@ -12,17 +19,35 @@ function onLinkedInLoad() {
 
 function onLinkedInAuth() {
 	$('#searchBar').html('<input style' + '="height: 24px;" class="box17" type="text" size="40">' +
-			' <button onclick="searchTitle()">Search Mentor</button></input>');
+	' <button onclick="resetSearch()">Search Mentor</button></input>');
 	IN.API.Profile("me")
-	.fields("firstName", "lastName", "positions", "industry", "headline")
+	.fields("firstName", "lastName", "id", "positions", "industry", "headline")
 	.result(displayProfiles);	 
 }
 
 /*
- * Self
+ * Search
  */
+function next(){
+	page+=1;
+	searchTitle();
+}
+
+function prev(){
+	page -= 1;
+	searchTitle();
+}
+
+function resetSearch(){
+	page = 0;
+	searchTitle();
+}
 
 function searchTitle(member){
+	console.log(member);
+	if(member == null){
+		member = memberSelf;
+	}
 	var keywords = $('input').val();
 	if(keywords == ""){
 		keywords = member.headline;
@@ -31,8 +56,8 @@ function searchTitle(member){
 	console.log(keywords);
 
 	IN.API.PeopleSearch()
-	.fields("firstName", "lastName", "positions", "industry", "siteStandardProfileRequest","public-profile-url")
-	.params({"keywords":  keywords})
+	.fields("firstName", "lastName", "id", "positions", "industry", "siteStandardProfileRequest","public-profile-url")
+	.params({"keywords":  keywords, "start":(start + (page*numPerPage)), "count":numPerPage, "sort":"relevance"})
 	.result(displayPeopleSearch)
 	.error(displayPeopleSearchError);
 }
@@ -82,11 +107,14 @@ function displayProfiles(profiles) {
 
 	var members = profiles.values;
 	for (var member in members) {
-		profilesDiv.innerHTML += "<p>Recommended mentors for " + members[member].firstName + " " + members[member].lastName + ":</p>"; 
+		profilesDiv.innerHTML += "<p>Recommended mentors for " + members[member].firstName + " " + 
+		members[member].lastName + ":</p>"; 
 		/*  + " in the " + members[member].industry + " industry.";*/
 	}
 
-	searchTitle(members[0]);
+	memberSelf = members[0];
+	memberId = memberSelf.id;
+	searchTitle(memberSelf);
 
 }
 
@@ -105,18 +133,37 @@ function displayPeopleSearch(peopleSearch) {
 	$('#peopleSearch').html("");
 	// Loop through the people returned
 	var members = peopleSearch.people.values;
-	//console.log(members);
-	var resultsHtml = "";
+	console.log(peopleSearch);
+	var start = peopleSearch.people._start + 1; // because humans count from 1, not 0.
+	var range = peopleSearch.people._start + peopleSearch.people._count;
+	var total = peopleSearch.people._total;
+	var resultsHtml = "<p>Displaying " + start + "-" + range + " of " + total + " results.</p>";
+	if(page > 0){
+		resultsHtml += '<button onclick="prev()">Prev</button>';		
+	}
+	if(range < total){
+		resultsHtml += '<button onclick="next()">Next</button>';	
+	}
+	resultsHtml += '<br>';
+
+
 	for (var member in members) {
 
+//		if(members[member].id != memberId)
+//		{
 		// Look through result to make name and url.
 		var nameText = members[member].firstName + " " + members[member].lastName;
+//		resultsHtml += '<p>' + nameText	+ ' works in the ' + members[member].industry + ' industry.</p>';
 		var url = members[member].publicProfileUrl;
 		resultsHtml += '<sc'+'ript type="IN/MemberProfile" data-id="'+url+'" data-format="inline" data-related="false"></sc'+'ript>';        
+//		}
 	}
+
 	$('#peopleSearch').append(resultsHtml);
-	//the below statement needs to be explicitly called to interpret and parse
-	//the inline script tags as the linked in api parses the dom onle on load.
+	/* 
+	 * the below statement needs to be explicitly called to interpret and parses 
+	 * the inline script tags as the linked in api parses the dom only on load.
+	 */
 	IN.parse($("#peopleSearch")[0]);
 
 }
